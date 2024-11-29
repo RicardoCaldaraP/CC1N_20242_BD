ccf7a5a6da4b275443d08da7711bfc68272b453e
Ricardo Caldara Pinto <ricardo.caldara@uvvnet.com.br>
2024-11-29 08:19:30 -0300

    Update EST_CASO_II.sql

--- DQL/EST_CASO_II.sql
+++ DQL/EST_CASO_II.sql
@@ -1,1 +1,24 @@
+SELECT Nome, CPF, DataNascimento, Telefone
+FROM Pacientes;
 
+SELECT c.Data, c.Horario, m.Nome AS Medico, c.Descricao
+FROM Consultas c
+JOIN Pacientes p ON c.PacienteID = p.ID
+JOIN Medicos m ON c.MedicoID = m.ID
+WHERE p.CPF = 'XXX.XXX.XXX-XX';
+
+SELECT m.Nome AS Medico, e.Nome AS Especialidade
+FROM Medicos m
+JOIN Especialidades e ON m.EspecialidadeID = e.ID;
+
+SELECT c.Data, c.Horario, p.Nome AS Paciente, m.Nome AS Medico
+FROM Consultas c
+JOIN Pacientes p ON c.PacienteID = p.ID
+JOIN Medicos m ON c.MedicoID = m.ID
+WHERE c.Data = '2024-12-01';
+
+SELECT p.Nome AS Paciente, c.Data, c.Horario
+FROM Consultas c
+JOIN Pacientes p ON c.PacienteID = p.ID
+JOIN Medicos m ON c.MedicoID = m.ID
+WHERE m.Nome = 'Dra. Ana Maria';
